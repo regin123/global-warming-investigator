be2e0e3e8fa73fc498998fe8030f3955039ddcc0
piotr <piotrpopis98@icloud.com>
2020-10-24 17:00:01 +0200

    asdf

--- best_hacks/info_map/views.py
+++ best_hacks/info_map/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+from django.template import loader
+
+
+def display_map(request):
+    template = loader.get_template('only_map.html')
+    response_body = template.render({'current_user': request.user})
+    return HttpResponse(response_body)
