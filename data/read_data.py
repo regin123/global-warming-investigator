2452c4468214b902766afa048f5f8f47955c32f6
regin <cicheckiigor@gmail.com>
2020-10-25 02:40:51 +0200

    add script used to read data

---
+++ data/read_data.py
@@ -0,0 +1,45 @@
+import json
+import pprint
+import pandas as pd
+
+pp = pprint.PrettyPrinter(indent=4)
+with open('map_dictionary.json') as json_file:
+    map_dictionary = json.load(json_file)
+
+with open('countries_dictionary.json') as json_file:
+    countries_dict = json.load(json_file)
+
+important_countries_name = []
+"""
+for country in data['2003']:
+    if country in countries_dict:
+        important_countries_name.append(country)
+"""
+
+data = pd.read_csv('data.csv')
+print(data.head())
+#pp.pprint(map_dictionary)
+#pp.pprint(data)
+#print(len(countries_dict))
+#print(len(important_countries_name))
+# for country in important_countries_name:
+#    print(country, countries_dict[country])
+#data.fillna(0)
+super_clean_data = {}
+for year in data.columns[2:]: 
+    super_clean_data[str(year)] = {"areas":{}}
+    for country_name in data["country_name"]:
+        # print(country_name)
+        if country_name in countries_dict:
+            country_code = countries_dict[country_name]
+            if country_code in map_dictionary:
+                super_clean_data[str(year)]["areas"][country_code] = {}
+                super_clean_data[str(year)]["areas"][country_code]['href'] = map_dictionary[country_code]['href']
+                super_clean_data[str(year)]["areas"][country_code]['tooltip'] = map_dictionary[country_code]['tooltip']
+                super_clean_data[str(year)]["areas"][country_code]['name'] = country_name
+                super_clean_data[str(year)]["areas"][country_code]['value'] = float(data.loc[data['country_name'] == country_name][str(year)].values) #data.loc[0:, ['country_code',str(1960)]] #data[str(year)][country_name]
+
+pp.pprint(super_clean_data['2003'])
+print(len(super_clean_data['2004']))
+with open('super_clean_data.json', 'w') as outfile:
+    json.dump(super_clean_data, outfile)
