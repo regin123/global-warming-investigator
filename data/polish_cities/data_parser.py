d23f4e56a3f89c6ab884de6f23f418b2bd806bb0
piotr <piotrpopis98@icloud.com>
2020-10-26 15:38:07 +0100

    code cleanup, new menubar, colors

--- data/polish_cities/data_parser.py
+++ data/polish_cities/data_parser.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 
@@ -13,10 +14,10 @@ def get_cities_data():
                 if values[2][-1] == 'N' or values[2][-1] == 'S':
                     lat = values[1]
                     deg, minutes, direction = re.split('[°\']', lat)
-                    lat_n = (float(deg) + float(minutes)/60) * (-1 if direction in ['W', 'S'] else 1)
+                    lat_n = (float(deg) + float(minutes) / 60) * (-1 if direction in ['W', 'S'] else 1)
                     longt = values[2]
                     deg, minutes, direction = re.split('[°\']', lat)
-                    longt_n = (float(deg) + float(minutes)/60) * (-1 if direction in ['W', 'S'] else 1)
+                    longt_n = (float(deg) + float(minutes) / 60) * (-1 if direction in ['W', 'S'] else 1)
                     dic[values[0]] = [lat_n, longt_n]
 
     return dic
